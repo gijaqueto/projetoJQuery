471ea93aa22386f326b9d6e49483e2de167b746b
Giovanna Jaqueto <gijaqueto1@gmail.com>
2024-01-14 18:31:02 -0300

    alteracoes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('header button').click(function () {
         $('form').slideDown();
     })
@@ -7,8 +7,20 @@ $(document).ready(function(){
         $('form').slideUp();
     })
 
-    $('form').on('sumbit', function (e) {
-        console.log("submit");
+    $('form').on('sumbit', function(e) {
         e.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real"> 
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
     })
 })
